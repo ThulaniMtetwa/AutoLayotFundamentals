f4e33c4de5cc72627aa8a8ffb2110db0d31b4c9e
Thulani Mtetwa <Thulani.Mtetwa@standardbank.co.za>
2023-04-19 15:53:03 +0200

    Initial Commit

---
+++ AutoLayotFundamentals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayotFundamentals
+//
+//  Created by Thulani Mtetwa on 2023/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
